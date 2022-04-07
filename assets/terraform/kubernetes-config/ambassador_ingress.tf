ad1a6268a2a5f9f113049512fb26d3ca5a553713
Vlad Ciobancai <ngagecj@gmail.com>
2022-04-07 09:33:21 +0300

    Updated nginx and added Ambassador first draft

--- assets/terraform/kubernetes-config/ambassador_ingress.tf
+++ assets/terraform/kubernetes-config/ambassador_ingress.tf
@@ -1,119 +1,28 @@
-resource "helm_release" "nginx_ingress" {
-  name      = "ingress-nginx"
-  namespace = "ingress-nginx"
+# data "http" "aes-crd" {
+#   url = "https://app.getambassador.io/yaml/edge-stack/2.1.2/aes-crds.yaml"
 
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  version          = "4.0.13"
-  create_namespace = true
+# }
 
-  set {
-    name  = "service.type"
-    value = "LoadBalancer"
-  }
-  set {
-    name  = "service.annotations.service\\.beta\\.kubernetes\\.io/do-loadbalancer-name"
-    value = format("%s-nginx-ingress", var.cluster_name)
-  }
-}
+# output "data_http" {
+#   value = data.http.aes-crd.body
+# }
 
-resource "kubernetes_namespace" "backend" {
-  metadata {
-    name = "backend"
-  }
-}
+# resource "kubectl_manifest" "aes-crd-manifest" {
+#   yaml_body = "https://app.getambassador.io/yaml/edge-stack/2.1.2/aes-crds.yaml"
+# }
 
-resource "kubernetes_deployment" "echo" {
-  metadata {
-    name      = "echo"
-    namespace = kubernetes_namespace.backend.metadata.0.name
-  }
-  spec {
-    replicas = 2
-    selector {
-      match_labels = {
-        app = "echo"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = "echo"
-        }
-      }
-      spec {
-        container {
-          image = "jmalloc/echo-server"
-          name  = "echo"
+# resource "helm_release" "ambassador_ingress" {
+#  name      = "ambassador"
+#  namespace = "ambassador"
 
-          port {
-            container_port = "8080"
-          }
+#  repository       = "https://app.getambassador.io"
+#  chart            = "edge-stack"
+#  version          = "7.2.2"
+#  create_namespace = true
 
-          resources {
-            limits = {
-              memory = "512M"
-              cpu    = "1"
-            }
-            requests = {
-              memory = "256M"
-              cpu    = "50m"
-            }
-          }
-        }
-      }
-    }
-  }
-}
+#    set {
+#      name  = "emissary-ingress.createDefaultListeners"
+#      value = "true"
+#    }
 
-resource "kubernetes_service" "echo" {
-  metadata {
-    name      = "echo-service"
-    namespace = kubernetes_namespace.backend.metadata.0.name
-  }
-  spec {
-    selector = {
-      app = kubernetes_deployment.echo.metadata.0.name
-    }
-
-    port {
-      port        = 80
-      target_port = 8080
-    }
-  }
-}
-
-resource "kubernetes_ingress" "test_ingress" {
-  wait_for_load_balancer = true
-  metadata {
-    name      = "test-ingress"
-    namespace = kubernetes_namespace.backend.metadata.0.name
-    annotations = {
-      "kubernetes.io/ingress.class"          = "nginx"
-      "ingress.kubernetes.io/rewrite-target" = "/"
-    }
-  }
-
-  spec {
-    rule {
-      http {
-        path {
-          backend {
-            service_name = kubernetes_service.echo.metadata.0.name
-            service_port = 80
-          }
-
-          path = "/test"
-        }
-      }
-    }
-  }
-}
-
-data "httpclient_request" "nginx_ingress" {
-  url = "http://${ kubernetes_ingress.test_ingress.status.0.load_balancer.0.ingress.0.ip }/test"
-}
-
-output "validate_nginx_ingress" {
-  value = data.httpclient_request.nginx_ingress.response_code
-}
+# }
--- assets/terraform/kubernetes-config/main.tf
+++ assets/terraform/kubernetes-config/main.tf
@@ -12,6 +12,18 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.0.1"
     }
+    httpclient = {
+      version = "0.0.3"
+      source  = "dmachard/http-client"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
   }
 }
 
@@ -44,108 +56,3 @@ provider "helm" {
   }
 }
 
-resource "kubernetes_namespace" "test" {
-  metadata {
-    name = "test"
-  }
-}
-
-resource "kubernetes_deployment" "test" {
-  metadata {
-    name      = "test"
-    namespace = kubernetes_namespace.test.metadata.0.name
-  }
-  spec {
-    replicas = 2
-    selector {
-      match_labels = {
-        app = "test"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = "test"
-        }
-      }
-      spec {
-        container {
-          image = "hashicorp/http-echo"
-          name  = "http-echo"
-          args  = ["-text=test"]
-
-          resources {
-            limits = {
-              memory = "512M"
-              cpu    = "1"
-            }
-            requests = {
-              memory = "256M"
-              cpu    = "50m"
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "test" {
-  metadata {
-    name      = "test-service"
-    namespace = kubernetes_namespace.test.metadata.0.name
-  }
-  spec {
-    selector = {
-      app = kubernetes_deployment.test.metadata.0.name
-    }
-
-    port {
-      port = 5678
-    }
-  }
-}
-
-resource "helm_release" "nginx_ingress" {
-  name      = "nginx-ingress-controller"
-  namespace = kubernetes_namespace.test.metadata.0.name
-
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx-ingress-controller"
-
-  set {
-    name  = "service.type"
-    value = "LoadBalancer"
-  }
-  set {
-    name  = "service.annotations.service\\.beta\\.kubernetes\\.io/do-loadbalancer-name"
-    value = format("%s-nginx-ingress", var.cluster_name)
-  }
-}
-
-resource "kubernetes_ingress" "test_ingress" {
-  wait_for_load_balancer = true
-  metadata {
-    name      = "test-ingress"
-    namespace = kubernetes_namespace.test.metadata.0.name
-    annotations = {
-      "kubernetes.io/ingress.class"          = "nginx"
-      "ingress.kubernetes.io/rewrite-target" = "/"
-    }
-  }
-
-  spec {
-    rule {
-      http {
-        path {
-          backend {
-            service_name = kubernetes_service.test.metadata.0.name
-            service_port = 5678
-          }
-
-          path = "/test"
-        }
-      }
-    }
-  }
-}
